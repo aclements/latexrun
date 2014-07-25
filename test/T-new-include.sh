11fa9a227a716db2bebd2a8eabb56d168a31d176
Austin Clements <amdragon@mit.edu>
2014-07-24 21:06:22 -0400

    Tests for re-running after missing inputs are created

---
+++ test/T-new-include.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Test that latexrun correctly re-runs latex after missing \include
+# files are created.
+
+set -e
+
+TMP=tmp.$(basename -s .sh $0)
+mkdir -p $TMP
+cat > $TMP/test.tex <<EOF
+\documentclass{article}
+
+\begin{document}
+\include{./$TMP/chap1}
+\end{document}
+EOF
+rm -f $TMP/chap1.tex
+
+echo chap1 does not exist
+"$@" $TMP/test.tex || true
+
+echo Hello > $TMP/chap1.tex
+
+echo chap1 now exists
+"$@" $TMP/test.tex
+
+rm -rf $TMP
+
+## output:
+## chap1 does not exist
+## tmp.T-new-include/test.tex: No file ./tmp.T-new-include/chap1.tex
+## tmp.T-new-include/test.tex: No pages (no output produced)
+## chap1 now exists
---
+++ test/T-new-input.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Test that latexrun correctly re-runs latex after missing \input
+# files are created.
+
+set -e
+
+TMP=tmp.$(basename -s .sh $0)
+mkdir -p $TMP
+cat > $TMP/test.tex <<EOF
+\documentclass{article}
+
+\begin{document}
+\input{./$TMP/chap1}
+\end{document}
+EOF
+rm -f $TMP/chap1.tex
+
+echo chap1 does not exist
+"$@" $TMP/test.tex || true
+
+echo Hello > $TMP/chap1.tex
+
+echo chap1 now exists
+"$@" $TMP/test.tex
+
+rm -rf $TMP
+
+## output:
+## chap1 does not exist
+## tmp.T-new-input/test.tex:4: error: [LaTeX] File `./tmp.T-new-input/chap1.tex' not found
+##       at <read *>
+##     from \input{./tmp.T-new-input/chap1}^^M
+##                                         ^
+## tmp.T-new-input/test.tex: error: ==> Fatal error occurred, no output PDF file produced!
+## chap1 now exists
